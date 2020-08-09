79ca4e1e0fe1d800da1411dd4eeff7d786fda383
Bruno Pinheiro <bruno.faria.pinheiro@gmail.com>
2020-08-09 16:43:05 -0300

    aula 28

--- src/test/java/br/com/devdojo/StudentEndpointTest.java
+++ src/test/java/br/com/devdojo/StudentEndpointTest.java
@@ -3,6 +3,7 @@ package br.com.devdojo;
 import br.com.devdojo.model.Student;
 import br.com.devdojo.repository.StudentRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
@@ -16,12 +17,16 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.springframework.http.HttpMethod.DELETE;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,6 +50,12 @@ public class StudentEndpointTest {
         public RestTemplateBuilder restTemplateBuilder() {
             return new RestTemplateBuilder().basicAuthentication("toyo", "devdojo");
         }
+    }
+
+    @Before
+    public void setup() {
+        Student student = new Student(1L, "Legolas", "legolas@lotr.com");
+        BDDMockito.when(studentRepository.findById(student.getId())).thenReturn(java.util.Optional.of(student));
     }
 
     @Test
@@ -74,9 +85,7 @@ public class StudentEndpointTest {
 
     @Test
     public void getStudentsByIdWhenUsernameAndPasswordAreCorrectShouldReturnStatusCode200() {
-        Student student = new Student(1L, "Legolas", "legolas@lotr.com");
-        BDDMockito.when(studentRepository.findById(student.getId())).thenReturn(java.util.Optional.of(student));
-        ResponseEntity<Student> response = restTemplate.getForEntity("/v1/protected/students/{id}", Student.class, student.getId());
+        ResponseEntity<Student> response = restTemplate.getForEntity("/v1/protected/students/{id}", Student.class, 1L);
         Assertions.assertThat(response.getStatusCodeValue()).isEqualTo(200);
     }
 
@@ -84,5 +93,52 @@ public class StudentEndpointTest {
     public void getStudentsByIdWhenUsernameAndPasswordAreCorrectAndStudentDoesNotExistShouldReturnStatusCode404() {
         ResponseEntity<Student> response = restTemplate.getForEntity("/v1/protected/students/{id}", Student.class, -1);
         Assertions.assertThat(response.getStatusCodeValue()).isEqualTo(404);
+    }
+
+    @Test
+    public void deleteWhenUserHasRoleAdminAndStudentExistsShouldReturnStatusCode200() {
+        BDDMockito.doNothing().when(studentRepository).deleteById(1L);
+        ResponseEntity<String> exchange = restTemplate.exchange("/v1/admin/students/{id}", DELETE, null, String.class, 1L);
+        Assertions.assertThat(exchange.getStatusCodeValue()).isEqualTo(200);
+    }
+
+    @Test
+    @WithMockUser(username = "xx", password = "xx", roles = {"USER", "ADMIN"})
+    public void deleteWhenUserHasRoleAdminAndStudentDoesNotExistShouldReturnStatusCode404() throws Exception {
+        BDDMockito.doNothing().when(studentRepository).deleteById(1L);
+//        ResponseEntity<String> exchange = restTemplate.exchange("/v1/admin/students/{id}", DELETE, null, String.class, -1L);
+//        Assertions.assertThat(exchange.getStatusCodeValue()).isEqualTo(404);
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/v1/admin/students/{id}", -1L))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser(username = "xx", password = "xx", roles = {"USER"})
+    public void deleteWhenUserDoesNotHaveRoleAdminShouldReturnStatusCode403() throws Exception {
+        BDDMockito.doNothing().when(studentRepository).deleteById(1L);
+//        ResponseEntity<String> exchange = restTemplate.exchange("/v1/admin/students/{id}", DELETE, null, String.class, -1L);
+//        Assertions.assertThat(exchange.getStatusCodeValue()).isEqualTo(404);
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/v1/admin/students/{id}", -1L))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void createWhenNameIsNullShouldReturnStatusCode400BadRequest() throws Exception {
+        Student student = new Student(3L, null, "sam@lotr.com");
+        BDDMockito.when(studentRepository.save(student)).thenReturn(student);
+        ResponseEntity<String> response = restTemplate.postForEntity("/v1/admin/students/", student, String.class);
+        Assertions.assertThat(response.getStatusCodeValue()).isEqualTo(400);
+        Assertions.assertThat(response.getBody()).contains("fieldMessage", "O campo nome do estudante é obrigatório");
+    }
+
+    @Test
+    public void createShouldPersistDataAndReturnStatusCode201() {
+        Student student = new Student(3L, null, "sam@lotr.com");
+        BDDMockito.when(studentRepository.save(student)).thenReturn(student);
+        ResponseEntity<Student> response = restTemplate.postForEntity("/v1/admin/students/", student, Student.class);
+        Assertions.assertThat(response.getStatusCodeValue()).isEqualTo(201);
+        Assertions.assertThat(response.getBody().getId()).isNotNull();
     }
 }
